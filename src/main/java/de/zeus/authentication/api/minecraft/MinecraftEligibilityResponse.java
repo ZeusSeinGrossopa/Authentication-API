c5f0963b1810144de16ba7be5fae4e2cdf547687
ZeusSeinGrossopa <zeusseingrossopa@gmail.com>
2022-08-15 23:12:48 +0200

    Added the MinecraftEligibilityResponse to get information when you created the minecraft account and get other information about the name

---
+++ src/main/java/de/zeus/authentication/api/minecraft/MinecraftEligibilityResponse.java
@@ -0,0 +1,31 @@
+package de.zeus.authentication.api.minecraft;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * This class gets information about the minecraft account if you can change your name and other information.
+ * See <a href="https://mojang-api-docs.netlify.app/needs-auth/name-change-eligibility.html">here</a> for the documentation.
+ * The URL is <a href="https://api.minecraftservices.com/minecraft/profile/namechange">...</a>
+ *
+ * @author ZeusSeinGrossopa
+ */
+public class MinecraftEligibilityResponse {
+
+    /**
+     * The time when you changed your name
+     */
+    @SerializedName("changedAt")
+    public String changedAt;
+
+    /**
+     * The time when you created your account
+     */
+    @SerializedName("createdAt")
+    public String accountCreatedAt;
+
+    /**
+     * If you are eligible to change your name
+     */
+    @SerializedName("nameChangeAllowed")
+    public boolean nameChangeAllowed;
+}
